ca28f5437a0fdaa86e43c2aa3390a2865ecaa826
millereg <gemillert4090@outlook.com>
2025-09-27 15:55:19 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://millereg.github.io',
+  base: 'astro_interno',
 });
